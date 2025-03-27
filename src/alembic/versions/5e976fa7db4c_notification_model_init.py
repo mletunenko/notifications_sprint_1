9b07520ceb016399778f672e7c30417eed94199b
mletunenko <mletunenko@gmail.com>
2025-03-27 12:39:23 +0800

    таблица для сохранения отправленных уведомлений

--- src/alembic/versions/5e976fa7db4c_notification_model_init.py
+++ src/alembic/versions/5e976fa7db4c_notification_model_init.py
@@ -8,9 +8,9 @@ Create Date: 2025-03-25 17:24:59.978950
 
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "5e976fa7db4c"
