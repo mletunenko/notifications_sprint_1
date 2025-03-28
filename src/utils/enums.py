6d256fd6daee2511f8f2eaff6d4012deb9d2802a
mletunenko <mletunenko@gmail.com>
2025-03-28 13:29:54 +0800

    порядок в utils

--- src/utils/enums.py
+++ src/utils/enums.py
@@ -1,10 +1,9 @@
 import enum
-from enum import Enum
 
 
-class ClientErrorMessage(Enum):
+class ClientErrorMessage(enum.StrEnum):
     NOT_FOUND_TEMPLATE_ERROR = "Шаблон не найден"
 
 
-class NotificationMethodEnum(enum.Enum):
+class NotificationMethodEnum(enum.StrEnum):
     EMAIL = "email"
