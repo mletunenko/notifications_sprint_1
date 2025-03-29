9d7eb96f6bee810c3976f8dbc6aef4549500db83
mletunenko <mletunenko@gmail.com>
2025-03-29 16:01:37 +0800

    перенос naming_conventions для моделей в код из конфига

--- src/models/base.py
+++ src/models/base.py
@@ -12,7 +12,14 @@ from core.config import settings
 
 class Base(DeclarativeBase):
     __abstract__ = True
-    metadata = MetaData(naming_convention=settings.db.naming_conventions)
+    naming_conventions: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+    metadata = MetaData(naming_convention=naming_conventions)
     id: Mapped[UUID4] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     created_at: Mapped[datetime.datetime] = mapped_column(
         TIMESTAMP(timezone=True), default=partial(datetime.datetime.now, datetime.timezone.utc)
